d79466bd2957b404b3a4f6f76979ba9a76751024
edwyn mckie <37442282+DarkShinobi100@users.noreply.github.com>
2021-03-04 15:37:05 +0000

    setting up the scene layout

---
+++ BehaviourTreeVsFuzzy/Assets/Scripts/ActionNode.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ActionNode : Node {
+
+    /* method sigrature for the action */
+    public delegate NodeStates ActionNodeDelegate();
+
+    /* The delgate that is called to evaluate this node */
+    private ActionNodeDelegate m_action;
+
+    /* Because this node contains no logic itself, the logic must 
+     * be passed in in the form of a delgate. As the signature states,
+     * the actopm needs to return a NodeStates enum */
+
+    public ActionNode(ActionNodeDelegate action)
+    {
+        m_action = action;
+    }
+
+    /*Evaluated the node using the passed in delegate and reports
+     * the resulting state as sppropriate */
+    public override NodeStates Evaluate()
+    {
+       switch (m_action())
+        {
+            case NodeStates.SUCCESS:
+                m_nodeState = NodeStates.SUCCESS;
+                return m_nodeState;
+
+            case NodeStates.FAILURE:
+                m_nodeState = NodeStates.FAILURE;
+                return m_nodeState;
+
+            case NodeStates.RUNNING:
+                m_nodeState = NodeStates.RUNNING;
+                return m_nodeState;
+
+            default:
+                m_nodeState = NodeStates.FAILURE;
+                return m_nodeState;
+        }
+    }
+}
