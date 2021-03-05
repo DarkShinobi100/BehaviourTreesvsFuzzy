c6c9f10f2ac55ce59775548b62a83941e1917f25
edwyn mckie <37442282+DarkShinobi100@users.noreply.github.com>
2021-03-05 13:03:17 +0000

    Code from book added

---
+++ BehaviourTreeVsFuzzy/Assets/Scripts/Game.cs
@@ -0,0 +1,42 @@
+﻿using UnityEngine;
+
+public class Game : MonoBehaviour {
+    [SerializeField]
+    private Animator stateMachine;
+    [SerializeField]
+    private PlayerController playerController;
+    [SerializeField]
+    private EnemyBehaviourTree enemyBehaviourTree;
+    [SerializeField]
+    private Player humanPlayer;
+    [SerializeField]
+    private Player aiPlayer;
+    [SerializeField]
+    private UIController uiController;
+    private int turn = 0;
+
+    private void Awake()
+    {
+        enemyBehaviourTree.SetPlayerData(humanPlayer, aiPlayer);
+        enemyBehaviourTree.onTreeExectuted += EndTurn;
+        playerController.onActionExecuted += EndTurn;
+    }
+
+    public void EvaluateAITree()
+    {
+        enemyBehaviourTree.Evaluate();
+    }
+
+    private void EndTurn()
+    {
+        if(humanPlayer.CurrentHealth <= 0 || aiPlayer.CurrentHealth <=0)
+        {
+            stateMachine.SetTrigger("EndGame");
+            uiController.EndGame();
+            return;
+        }
+        stateMachine.SetTrigger("EndTurn");
+        turn ^= 1;
+        uiController.SetTurn(turn);
+    }
+}
