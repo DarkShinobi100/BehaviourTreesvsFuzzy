ab559492c431c05379dbfc306c704e3a5d2d9d8a
edwyn mckie <37442282+DarkShinobi100@users.noreply.github.com>
2021-03-05 17:13:40 +0000

    New player script created with new functionality

---
+++ BehaviourTreeVsFuzzy/Assets/Scripts/Behaviour Trees/UpdatedEnemyBehaviorTree.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class UpdatedEnemyBehaviorTree : MonoBehaviour {
+
+    //necessary data
+    private Player playerData;
+    private Player ownData;
+
+    //nodes
+    public RandomBinaryNode buffCheckRandomNode;
+    public ActionNode buffCheckNode;
+    public ActionNode healthCheckNode;
+    public ActionNode attackCheckNode;
+    public Sequence buffCheckSequence;
+    public Selector rootNode;
+
+    //called when Tree has reached a result
+    public delegate void TreeExecuted();
+    public event TreeExecuted onTreeExecuted;
+
+    public delegate void NodePassed(string trigger);
+}
---
+++ BehaviourTreeVsFuzzy/Assets/Scripts/NewPlayer.cs
@@ -0,0 +1,143 @@
+﻿using UnityEngine;
+
+public class NewPlayer : MonoBehaviour
+{
+    [SerializeField]
+    private int maxHealth = 20;
+
+    [SerializeField]
+    private int currentHealth;
+
+    [SerializeField]
+    private int maxMana = 20;
+
+    [SerializeField]
+    private int currentMana;
+
+    [SerializeField]
+    private int AttackValue;
+
+    [SerializeField]
+    private int DefenceValue;
+
+    [SerializeField]
+    private int MagicAttackValue;
+
+    //State rules for AI
+    [SerializeField]
+    private int lowHealthThreshold = 7;
+    [SerializeField]
+    private int lowManaThreshold = 5;
+    [SerializeField]
+    private int lowDefenceThreshold = 3;
+    [SerializeField]
+    private int lowAttackThreshold = 2;
+
+    [Header("Ability Parameters")]
+    private int minHealAmount = 2;
+    private int maxHealAmount = 5;
+
+    //player values
+    public int CurrentHealth
+    {
+        get { return currentHealth; }
+    }
+    public int CurrentMana
+    {
+        get { return currentMana; }
+    }
+    public int CurrentDefence
+    {
+        get { return DefenceValue; }
+    }
+    public int CurrentAttack
+    {
+        get { return AttackValue; }
+    }
+    public int CurrentMagicAttack
+    {
+        get { return MagicAttackValue; }
+    }
+
+    //Triggers for decisions
+    public bool HasLowDefence
+    {
+        get { return DefenceValue < lowDefenceThreshold; }
+    }
+
+    public bool HasLowMana
+    {
+        get { return currentMana < lowManaThreshold; }
+    }
+
+    public bool HasLowAttack
+    {
+        get { return AttackValue < lowAttackThreshold; }
+    }
+
+    public bool HasLowHealth
+    {
+        get { return currentHealth < lowHealthThreshold; }
+    }
+
+    private void Awake()
+    {
+        //set player stats
+        currentHealth = maxHealth;
+        currentMana = maxMana;
+        AttackValue = 2;
+        MagicAttackValue = 3;
+        DefenceValue = 1;
+    }
+
+    public int Heal()
+    {
+        int healAmount = Random.Range(minHealAmount, maxHealAmount);
+        currentHealth += healAmount;
+        return currentHealth;
+    }
+
+    public int Damage(int receivedDamage)
+    {
+        int damageAmount = DefenceValue - receivedDamage;
+        DefenceValue--;
+        if (damageAmount<=0)
+        {
+            damageAmount = 2;
+        }
+        if(DefenceValue <0)
+        {
+            DefenceValue = 1;
+        }
+        currentHealth -= damageAmount;
+        return currentHealth;
+    }
+
+    public int increaseAttack()
+    {
+        AttackValue++;
+        return AttackValue;
+    }
+
+    public int increaseDefence()
+    {
+        DefenceValue++;
+        return DefenceValue;
+    }
+
+    public int increaseMagicAttack()
+    {
+        MagicAttackValue++;
+        return MagicAttackValue;
+    }
+
+    public int increaseMana()
+    {
+        currentMana = currentMana+5;
+        if(currentMana>maxMana)
+        {
+            currentMana = maxMana;
+        }
+        return currentMana;
+    }
+}
